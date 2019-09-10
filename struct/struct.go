e6883384e1865b0679fe10f390694996163546ee
Mohammed Ammar Kurabi <kurabi@gmail.com>
2019-09-10 13:06:36 +0400

    Adding an array of int struct

--- struct/struct.go
+++ struct/struct.go
@@ -6,6 +6,7 @@ type rectangle struct {
 	length  int
 	breadth int
 	color   string
+	x       []int
 }
 
 func (r *rectangle) print() {
@@ -13,22 +14,22 @@ func (r *rectangle) print() {
 }
 
 func main() {
-	var rect1 = &rectangle{10, 20, "Green"} // Can't skip any value
-	fmt.Println(rect1)
+	// var rect1 = &rectangle{10, 20, "Green"} // Can't skip any value
+	// fmt.Println(rect1)
 
-	var rect2 = &rectangle{}
-	rect2.length = 10
-	rect2.color = "Red"
-	fmt.Println(rect2) // breadth skipped
+	// var rect2 = &rectangle{}
+	// rect2.length = 10
+	// rect2.color = "Red"
+	// fmt.Println(rect2) // breadth skipped
 
-	var rect3 = &rectangle{}
-	(*rect3).breadth = 10
-	(*rect3).color = "Blue"
-	// fmt.Println(rect3) // length skipped
+	// var rect3 = &rectangle{}
+	// (*rect3).breadth = 10
+	// (*rect3).color = "Blue"
+	// // fmt.Println(rect3) // length skipped
 
-	rect3.print()
+	// rect3.print()
 
-	rect4 := rectangle{breadth: 20, color: "Yellow", length: 55}
+	rect4 := rectangle{breadth: 20, color: "Yellow", length: 55, x: []int{1, 2, 3}}
 
 	// fmt.Println(rect4)
 	rect4.print()
